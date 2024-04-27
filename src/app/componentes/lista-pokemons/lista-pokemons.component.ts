2fb01bc7e7ebbb85e8c32397831ab92a01d2424e
Yady <yadytica@gmail.com>
2024-04-27 17:26:01 +0200

    Creados los componentes principales

---
+++ src/app/componentes/lista-pokemons/lista-pokemons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-pokemons',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-pokemons.component.html',
+  styleUrl: './lista-pokemons.component.css'
+})
+export class ListaPokemonsComponent {
+
+}
