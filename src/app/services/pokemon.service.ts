af8e639b7e0cbc3efce976ed9e3f2a247efeae06
Yady <yadytica@gmail.com>
2024-04-27 17:55:21 +0200

    Creado el servicio

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {ApiResultListPokemonTCG, ApiResultOnePokemonTCG} from "../common/interfacesPokemonTCG";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  private URI: string = 'http://pokeapi.co/api/v2/cards';
+  private http: HttpClient = inject(HttpClient);
+
+  getPokemons():Observable<ApiResultListPokemonTCG> {
+
+    return this.http.get<ApiResultListPokemonTCG>(`${this.URI}/pokemons`);
+  }
+  getPokemon(id: string): Observable<ApiResultOnePokemonTCG> {
+
+    return this.http.get<ApiResultOnePokemonTCG>(`${this.URI}/${id}`);
+  }
 }
