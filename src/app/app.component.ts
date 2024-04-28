926521dd35fe15bbba79b89c6be7a948b769aab4
Daniel <daniel.garciaortega@cieep.com>
2024-04-28 03:51:36 +0200

    Añadidos estilos y contenido visual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./componentes/header/header.component";
 import { FooterComponent } from "./componentes/footer/footer.component";
+import { TabVerticalComponent } from "./componentes/tab-vertical/tab-vertical.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent]
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, TabVerticalComponent]
 })
 export class AppComponent {
   title = 'ProyectoAngular';
---
+++ src/app/componentes/tab-vertical/tab-vertical.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-tab-vertical',
+  standalone: true,
+  imports: [],
+  templateUrl: './tab-vertical.component.html',
+  styleUrl: './tab-vertical.component.css',
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class TabVerticalComponent {
+
+}
