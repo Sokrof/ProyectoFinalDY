b20814ca981399eeb128df375761363da3226022
Daniel <daniel.garciaortega@cieep.com>
2024-04-28 14:50:07 +0200

    Revisión 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
-
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Ruta por defecto o vacía, nos redirige a 'inicio'
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+    // Ruta para 'inicio' que carga el componente InicioComponent
+    { path: 'inicio', component: InicioComponent }
+];
